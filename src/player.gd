65b14da0102e423264e38d19be0bea3c00cea204
acid-rock <138583992+acid-rock@users.noreply.github.com>
2026-05-03 19:49:04 +0800

    feat (player): added attack animation

--- src/player.gd
+++ src/player.gd
@@ -6,11 +6,45 @@ var hitbox_offset: Vector2
 
 @onready var sprite = $AnimatedSprite2D
 @onready var footsteps = $AudioStreamPlayer2D
+@onready var hitbox: Area2D = $Hitbox
+
+var is_attacking = false
 
 func _physics_process(_delta) -> void:
-	process_movement()
-	process_animation()
+	if not is_attacking:
+		process_movement()
+		process_animation()
+	else:
+		velocity = Vector2.ZERO
+	
+	handle_attack()
 	move_and_slide()
+	
+func handle_attack() -> void:
+	if is_attacking:
+		return
+		
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		start_attack()
+		
+func start_attack() -> void:
+	is_attacking = true
+	play_animation("attack", last_direction)
+	enable_hitbox()
+	
+func enable_hitbox() -> void:
+	hitbox.monitoring = true
+	
+	if abs(last_direction.x) >= abs(last_direction.y):
+		if last_direction.x > 0:
+			hitbox.position = Vector2(20, 0)
+		else:
+			hitbox.position = Vector2(-20, 0)
+	else:
+		if last_direction.y > 0:
+			hitbox.position = Vector2(0, 20)
+		else:
+			hitbox.position = Vector2(0, -20)
 
 func process_movement() -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -41,3 +75,9 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 		else:
 			sprite.play(prefix + "_back")
 			
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation.begins_with("attack"):
+		is_attacking = false
+		hitbox.monitoring = false
+		play_animation("idle", last_direction)
